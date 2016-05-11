b6057774608f0e49661ae9bc4f594b9a3a9171ef
hcconnor <hamiltoncconnor@gmail.com>
2016-05-11 13:27:18 -0700

    Some small changes by Hamilton

--- Particle1.js
+++ Particle1.js
@@ -1,12 +1,12 @@
-$.getScript("engine.js", function(){
-	console.log("Game engine loaded");
-});
-$.getScript("ItemBase.js", function(){
-	console.log("Draggables loaded");
-});
-$.getScript("ShipLifetime.js", function(){
-	console.log("Ship loaded");
-});
+// $.getScript("engine.js", function(){
+// 	console.log("Game engine loaded");
+// });
+// $.getScript("ItemBase.js", function(){
+// 	console.log("Draggables loaded");
+// });
+// $.getScript("ShipLifetime.js", function(){
+// 	console.log("Ship loaded");
+// });
 
 var particles = {};
 var pW = 30;
@@ -17,6 +17,5 @@ function spriteImage(){
 	sources.push("http://i.imgur.com/r0sPSFb.png") // color wheel test sprite
 	sources.push("http://i.imgur.com/x3cSD4r.png") // star token test sprite
 	for (var i = 0; sources.length; i++) {
-		
-	}
 
+	}
