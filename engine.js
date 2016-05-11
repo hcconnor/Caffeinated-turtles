d8b10ac4694b1838f641225b8a474c472214b511
Whotsit <jacob.phong.le@gmail.com>
2016-05-11 14:37:11 -0700

    Updated GUI and added sprites

--- engine.js
+++ engine.js
@@ -119,7 +119,8 @@ function gui (sprite){
 	this.bar_height = 25;
 
 	this.draw = function(){
-		contex.drawImage(this.overlay, 0, 0);
+		contex.drawImage(this.overlay, 0, 0);\
+		context.fillStyle = "#4FFF52";
 		contex.fillRect(600, 450, this.bar_length * life_support/100, this.bar_height);
 		contex.fillRect(600, 500, this.bar_length * durability/100, this.bar_height);
 		contex.fillRect(600, 550, this.bar_length * happiness/100, this.bar_height);
