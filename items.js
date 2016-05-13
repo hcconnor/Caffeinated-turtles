b2b869ece19ba1f175c2cbbc82febd0a5c86051f
Alexander Mao <acmao@ucsc.edu>
2016-05-13 00:30:46 -0700

    Added four basic parts

--- items.js
+++ items.js
@@ -1,3 +1,9 @@
-rocketThruster = new Item(){
-
+rocketThruster = new Item("propulsion","Standard Thruster",100){
+  this.efficiency = 10;
 }
+
+supplyCrate = new Item("system","Supply Crate",100);
+
+fuelTank = new Item("fuel","Rocket Fuel Tank",100);
+
+iceCream = new Item("vanity","Ice Cream Machine",100);
