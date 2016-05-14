423228c10b613fcbd4406701d2d64dac2babaf0c
Alexander Mao <acmao@ucsc.edu>
2016-05-14 10:30:35 -0700

    Updated to take in sources

--- ItemBase.js
+++ ItemBase.js
@@ -1,11 +1,11 @@
 
-//Item([consumable/part/vanity, name, durability value)
+//Item([consumable/part/vanity, name, durability value, src)
 //returns an object item
-function Item(type, name, durability){
-	this.height = image.height;
-	this.width = image.width;
+function Item(type, name, durability, src){
 	this.type = type;
 	this.level = level;
 	this.name = name;
 	this.durability = durability;
+	this.image = new image;
+	this.image.src = src;
 }
