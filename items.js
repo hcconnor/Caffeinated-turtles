c2d63f5e9de23048d320a21d677c852612e2d6ad
Alexander Mao <acmao@ucsc.edu>
2016-05-16 00:50:55 -0700

    Modified Item Source and added cats and ice cream

--- items.js
+++ items.js
@@ -2,12 +2,12 @@ low = 50;
 mid = 100;
 high = 200;
 
-rocketThruster = new Item("propulsion","Standard Thruster",100, "https://drive.google.com/open?id=0BwnCI9LxiMD0bkdHd2dGR3ZRQUU"){
+rocketThruster = new Item("propulsion","Standard Thruster",100, "sprites/Thruster.png"){
   this.efficiency = 10;
 }
 
-supplyCrate = new Item("system","Supply Crate",100);
+supplyCrate = new Item("system","Supply Crate",100, "sprites/Storage.png");
 
-fuelTank = new Item("fuel","Rocket Fuel Tank",100);
+fuelTank = new Item("fuel","Rocket Fuel Tank",100, "sprites/Thruster.png");
 
-iceCream = new Item("vanity","Ice Cream Machine",100);
+iceCream = new Item("vanity","Ice Cream Machine",100, "sprites/IceCreamMachine.png");
