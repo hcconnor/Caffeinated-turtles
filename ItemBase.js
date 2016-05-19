247d933c8ce9ee741600fee92e263531b5d6b0be
Alexander Mao <acmao@ucsc.edu>
2016-05-18 17:12:04 -0700

    Item Damage

--- ItemBase.js
+++ ItemBase.js
@@ -3,13 +3,11 @@
 //returns an object item
 function Item(type, name, durability, src){
 	this.type = type;
-	this.level = level;
 	this.name = name;
 	this.durability = durability;
 	this.maxDurability = durability;
-	this.image = new image;
-	this.image.src = src;
 	this.damageLevel = 0;
+	this.src = src;
 
 	this.updateDurab = function(){
 		this.durability --;
@@ -22,10 +20,8 @@ function Item(type, name, durability, src){
 		}else if(this.durability <= 0){
 			this.damageLevel = 4;
 		}
+	};
 }
-
-var thruster = Item.prototype;
-thruster.efficiency = 0;
 
 //fix a part by dragging a part onto another that is already slotted into the ship
 function fix(part1, part2){
--- items.js
+++ items.js
@@ -2,10 +2,10 @@ var low = 50;
 var mid = 100;
 var high = 200;
 
-rocketThruster = new thruster("propulsion","Standard Thruster",100, "sprites/Thruster.png");
+rocketThruster = new thruster("propulsion","Standard Thruster",100);
 
-supplyCrate = new Item("system","Supply Crate",100, "sprites/Storage.png");
+supplyCrate = new Item("system","Supply Crate",100);
 
-fuelTank = new Item("fuel","Rocket Fuel Tank",100, "sprites/Thruster.png");
+fuelTank = new Item("fuel","Rocket Fuel Tank",100);
 
-iceCream = new Item("vanity","Ice Cream Machine",100, "sprites/IceCreamMachine.png");
+iceCream = new Item("vanity","Ice Cream Machine",100);
