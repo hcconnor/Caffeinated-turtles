146d99f8d8c5872baf4c47d663a70bc1283fb9fa
Alexander Mao <acmao@ucsc.edu>
2016-05-25 13:43:28 -0700

    Implemented other mp3 files

--- sounds.js
+++ sounds.js
@@ -1,5 +1,6 @@
 var soundFX = function(){
   this.panic = new Audio("sounds/panic.mp3");
+  this.menuSelect = new Audio("sounds/menu_select.mp3")
 
   this.play = function(sound){
     sound.play();
