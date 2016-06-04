9087dca59c8beacaf155ff14f62ad8b0462c9dfe
Alexander Mao <acmao@ucsc.edu>
2016-06-04 12:05:39 -0700

    Added planet warning and updated visual

--- distanceCalculator.js
+++ distanceCalculator.js
@@ -1,5 +1,5 @@
 function calcLittleShipPos(currentDist,totalDist, lineWidth){
-  var position = 400;
+  var position = 0;
   var dist = currentDist;
   var rposition = (dist*lineWidth)/totalDist;
   position += rposition;
@@ -12,15 +12,17 @@ function littleShip(src,y,height,width){
   this.image = new Image();
   this.image.src = src;
   this.y = y;
-  this.x = calcLittleShipPos(distance, 20000, 900);
+  this.x = calcLittleShipPos(distance, 10000, 1300);
   this.height = height;
   this.width = width;
 
   this.update = function(){
-    this.x = calcLittleShipPos(distance, 20000, 900);
+    this.x = calcLittleShipPos(distance, 10000, 1300);
   }
 
   this.draw = function(){
     context.drawImage(this.image,this.x,this.y,this.width,this.height);
+    context.fillStyle = "#ffffff";
+    context.fillText(distance, this.x, this.Y - 10);
   }
 }
